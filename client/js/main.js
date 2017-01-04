0996d6f66959c5eaf66748e57043ec27c51a4059
grpssinggames <grossinggames@gmail.com>
2017-01-04 21:33:35 +0300

    Внесены правки

--- client/js/main.js
+++ client/js/main.js
@@ -104,7 +104,7 @@ window.onload = function () {
 
                         ObjAnimate("spr_bottle_floor_bottle", "angle", 0, 0, function() { }, [ 
                             0,0,btn.angle % 360,
-                            2,0,newAngle - 1220,
+                            2,0,newAngle - 1020,
                             3,0,newAngle - 820,
                             4,0,newAngle - 360,
                             4.75,0,newAngle
