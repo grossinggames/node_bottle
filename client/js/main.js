55d7a12c0cb9f91fb5db8863a41c7d5efc06230f
grpssinggames <grossinggames@gmail.com>
2017-06-05 22:23:30 +0300

    Правки

--- client/js/main.js
+++ client/js/main.js
@@ -480,11 +480,11 @@ window.onload = function () {
             },
             event_mleave: function() {
                 //ButtonLeave("spr_bottle_button_change_table");
-                ObjSet("spr_bottle_button_change_table", { drawoff_x: 0 });
+                //ObjSet("spr_bottle_button_change_table", { drawoff_x: 0 });
             },
             event_menter: function() {
                 //ButtonEnter("spr_bottle_button_change_table");
-                ObjSet("spr_bottle_button_change_table", { drawoff_x: -35 });
+                //ObjSet("spr_bottle_button_change_table", { drawoff_x: -35 });
             }
         });
 
