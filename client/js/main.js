d82dd7d5755f711c16665fa93159d88c6dacd0f5
grossinggames <grossinggames@gmail.com>
2017-08-28 10:02:35 +0300

    Правки

--- client/js/main.js
+++ client/js/main.js
@@ -680,11 +680,13 @@ window.onload = function () {
                 HideModalWindow();
             },
             event_mleave: function() {
+                console.log('spr_interface_modalwindow_rating_close leave');
                 //ButtonLeave("spr_interface_modalwindow_rating_close");
                 ObjAnimate('spr_interface_modalwindow_change_bottle_close', "scale_x", 0, 0, function() {}, [ 0,0,'cur', 0.2,0,0.8 ]);
                 ObjAnimate('spr_interface_modalwindow_change_bottle_close', "scale_y", 0, 0, function() {}, [ 0,0,'cur', 0.2,0,0.8 ]);
             },
             event_menter: function() {
+                console.log('spr_interface_modalwindow_rating_close enter');
                 //ButtonEnter("spr_interface_modalwindow_rating_close");
                 ObjAnimate('spr_interface_modalwindow_change_bottle_close', "scale_x", 0, 0, function() {}, [ 0,0,'cur', 0.2,0,1 ]);
                 ObjAnimate('spr_interface_modalwindow_change_bottle_close', "scale_y", 0, 0, function() {}, [ 0,0,'cur', 0.2,0,1 ]);
