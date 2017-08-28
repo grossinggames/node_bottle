f79c2c78ac0d6b7d9865199460594e3fa680c6b0
grossinggames <grossinggames@gmail.com>
2017-08-28 10:14:39 +0300

    Правки

--- client/js/main.js
+++ client/js/main.js
@@ -685,13 +685,15 @@ window.onload = function () {
                 // ObjAnimate('spr_interface_modalwindow_change_bottle_close', "scale_x", 0, 0, function() {}, [ 0,0,'cur', 0.2,0,0.8 ]);
                 // ObjAnimate('spr_interface_modalwindow_change_bottle_close', "scale_y", 0, 0, function() {}, [ 0,0,'cur', 0.2,0,0.8 ]);
                 ObjSet('spr_interface_modalwindow_change_bottle_close', { scale_x: 0.8, scale_y: 0.8 });
+                ObjAnimate('spr_interface_modalwindow_change_bottle_close', "alp", 0, 0, function() {  }, [ 0,0,ObjGet('spr_interface_modalwindow_change_bottle_close').scale_x, 1,0,1 ]);
             },
             event_menter: function() {
                 console.log('spr_interface_modalwindow_rating_close enter');
                 //ButtonEnter("spr_interface_modalwindow_rating_close");
                 // ObjAnimate('spr_interface_modalwindow_change_bottle_close', "scale_x", 0, 0, function() {}, [ 0,0,'cur', 0.2,0,1 ]);
                 // ObjAnimate('spr_interface_modalwindow_change_bottle_close', "scale_y", 0, 0, function() {}, [ 0,0,'cur', 0.2,0,1 ]);
-                ObjSet('spr_interface_modalwindow_change_bottle_close', { scale_x: 1, scale_y: 1 });
+                // ObjSet('spr_interface_modalwindow_change_bottle_close', { scale_x: 1, scale_y: 1 });
+                ObjAnimate('spr_interface_modalwindow_change_bottle_close', "alp", 0, 0, function() {  }, [ 0,0,ObjGet('spr_interface_modalwindow_change_bottle_close').scale_x, 1,0,0 ]);
             }
         });
 
