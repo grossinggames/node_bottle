088b872b2ae5b559105ad62a98aa6faae13af4ef
grossinggames <grossinggames@gmail.com>
2017-08-29 14:13:59 +0300

    Правки

--- modules/db/db.js
+++ modules/db/db.js
@@ -19,6 +19,30 @@ promise.then(function(db) {
     console.log('Promise db: ');
 });
 
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {  
+    console.log('Mongoose default connection open to ');
+  }); 
+  
+  // If the connection throws an error
+  mongoose.connection.on('error',function (err) {  
+    console.log('Mongoose default connection error: ' + err);
+  }); 
+  
+  // When the connection is disconnected
+  mongoose.connection.on('disconnected', function () {  
+    console.log('Mongoose default connection disconnected'); 
+  });
+  
+  // If the Node process ends, close the Mongoose connection 
+  process.on('SIGINT', function() {  
+    mongoose.connection.close(function () { 
+      console.log('Mongoose default connection disconnected through app termination'); 
+      process.exit(0); 
+    }); 
+  }); 
+
 // var Cat = mongoose.model('Cat', { name: String });
 
 // var kitty = new Cat({ name: 'Zildjian' });
