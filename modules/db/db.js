87dee677a4effabb65d94224f20b47a42e257b4c
grossinggames <grossinggames@gmail.com>
2017-08-29 12:27:06 +0300

    Правки

--- modules/db/db.js
+++ modules/db/db.js
@@ -2,3 +2,16 @@
 // Получить рейтинг игроков
 // db.getCollection('vk_bottle').find({}).sort({kiss: -1}).limit(10);
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vk_bottle');
+
+// var Cat = mongoose.model('Cat', { name: String });
+
+// var kitty = new Cat({ name: 'Zildjian' });
+// kitty.save(function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('meow');
+//   }
+// });
